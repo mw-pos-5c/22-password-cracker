e4c68d65166fd8aa7055a27d0fd2990532a8a282
VarChar42 <michw2014@gmail.com>
2022-02-12 12:33:33 +0100

    update

---
+++ Password.Cracker.Cli/Password.Cracker.Cli/CrackUtils.cs
@@ -0,0 +1,147 @@
+﻿#region usings
+
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+
+#endregion
+
+namespace Password.Cracker.Cli;
+
+public class CrackUtils
+{
+    #region Constants and Fields
+
+    private readonly byte[] alphabet;
+    private readonly int length;
+    private readonly byte[] target;
+
+    #endregion
+
+    public CrackUtils(byte[] alphabet, int length, byte[] target)
+    {
+        this.alphabet = alphabet;
+        this.length = length;
+        this.target = target;
+
+        Total = (long) Math.Pow(alphabet.Length, length);
+    }
+
+    public bool Found { get; private set; }
+    public string Result { get; private set; }
+    public long Total { get; }
+    public long TotalHashed { get; private set; }
+
+    public byte[] CalcStart(long start)
+    {
+        long from = start;
+        long basis = alphabet.Length;
+
+        var result = new byte[length];
+
+        for (var i = 0; i < result.Length; i++)
+        {
+            result[i] = (byte) (from % basis);
+            from /= basis;
+        }
+
+        return result;
+    }
+
+    public void Crack(long from, long to)
+    {
+        var sha256 = SHA256.Create();
+
+        byte[] currentTry = CalcStart(from);
+        var result = new byte[length];
+
+        for (var i = 0; i < result.Length; i++)
+        {
+            result[i] = alphabet[0];
+        }
+
+        for (var x = 0; x < to - from; x++)
+        {
+            if (Found) return;
+            
+            byte[] hash = sha256.ComputeHash(result);
+            ++TotalHashed;
+
+            if (CmpBytes(hash, target))
+            {
+                Found = true;
+                Result = Encoding.UTF8.GetString(result);
+                return;
+            }
+
+            ++currentTry[0];
+            for (var i = 0; i < length; i++)
+            {
+                if (currentTry[i] == alphabet.Length)
+                {
+                    currentTry[i] = 0;
+
+                    if (i == length - 1)
+                        return;
+
+                    ++currentTry[i + 1];
+                    result[i] = alphabet[currentTry[i]];
+                }
+                else
+                {
+                    result[i] = alphabet[currentTry[i]];
+                    break;
+                }
+            }
+        }
+    }
+
+    public async Task<string> CrackAsync(int count)
+    {
+        long piece = Total / count;
+
+        var tasks = new Task[count];
+
+        long from;
+        long to = 0;
+
+        for (var i = 0; i < count; i++)
+        {
+            from = to;
+            to += piece;
+            long from1 = from;
+            long to1 = to;
+            tasks[i] = Task.Run(() => Crack(from1, to1));
+        }
+
+        await Task.WhenAll(tasks);
+        return Result;
+    }
+
+    public static byte[] HexStringToByte(string hex)
+    {
+        var data = new byte[hex.Length / 2];
+        for (var i = 0; i < data.Length; i++)
+        {
+            string byteValue = hex.Substring(i * 2, 2);
+            data[i] = byte.Parse(byteValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+        }
+
+        return data;
+    }
+
+    public static byte[] PrepareAlphabet(string input)
+    {
+        return Encoding.UTF8.GetBytes(input.ToCharArray());
+    }
+
+    private bool CmpBytes(byte[] arr1, byte[] arr2)
+    {
+        for (var i = 0; i < arr1.Length; i++)
+        {
+            if (arr1[i] != arr2[i]) return false;
+        }
+
+        return true;
+    }
+}
---
+++ Password.Cracker.Cli/Password.Cracker.Cli/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+
+namespace Password.Cracker.Cli;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+        
+        byte[] pw0 = CrackUtils.HexStringToByte("A746222F09D85605C52D4E636788D6FFDC274698B98B8C5F3244C06958683A69");
+        byte[] pw1 = CrackUtils.HexStringToByte("3086E346353248775A2C5D74E36A9C9B9BD226A1EE401F830AC499633DC00031");
+        byte[] pw2 = CrackUtils.HexStringToByte("26775436073E00D207E192857EE3730CFCA19DE16F01F0780096EF217C2919EF");
+        byte[] pw3 = CrackUtils.HexStringToByte("43C19A093B34B581DDCC7207F6BD094F6940DB69F035C444425ED84D2CAC037D");
+        
+        int length;
+        byte[] alphabet;
+        byte[] pw;
+        
+        
+        // pw0 - snow
+        // length = 4;
+        // alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        // pw = pw0;
+        
+        
+        // pw1 - HTLGKR
+         length = 6;
+         alphabet = CrackUtils.PrepareAlphabet("abcdefghijklmnopqrstuvwxyz".ToUpper());
+         pw = pw1;
+        
+        // p2 - CoV19
+        //length = 5;
+        //alphabet = ShaUtils.PrepareAlphabet("abcdefghijklmnopqrstuvwxyz".ToUpper() + "abcdefghijklmnopqrstuvwxyz" + "0123456789");
+        //pw = pw2;
+
+
+        var crack = new CrackUtils(alphabet, length, pw);
+        
+        //crack.Crack(0, 100);
+        
+        
+        Task.Run(() =>
+        {
+            long last = 0;
+            
+            Process proc = Process.GetCurrentProcess();
+            
+            while (!crack.Found)
+            {
+                long now = crack.TotalHashed;
+                long diff = now-last;
+                last = now;
+                Console.WriteLine($"Status: {now} | {diff} | MEM "+((double)proc.PrivateMemorySize64 / 1000000));
+                Thread.Sleep(1000);
+            }
+        });
+
+        int threadCount = Environment.ProcessorCount;
+
+        var stopwatch = new Stopwatch();
+        
+        stopwatch.Start();
+        string result = crack.CrackAsync(threadCount).Result;
+        stopwatch.Stop();
+
+        Console.WriteLine("Result: "+result);
+        
+        Console.WriteLine("Took: "+stopwatch.ElapsedMilliseconds+" ms");
+        
+
+    }
+}
